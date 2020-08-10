bbdeb3517ff4bf9f9ccb9a6124d2929d4e423605
tifftech <tiffanimelissabest@gmail.com>
2020-08-10 09:49:26 -0400

    cc

--- app/models/file.rb
+++ app/models/file.rb
@@ -1,1 +1,67 @@
 # create files for your ruby classes in this directory
+
+class Bakery
+
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name) 
+        @name = name
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def desserts 
+        Dessert.all.select{ |dessert| dessert.bakery == self }
+    end
+
+    def ingredients 
+        self.desserts.map { |dessert| dessert.ingredients }
+    end
+
+end
+
+class Dessert
+
+    attr_accessor :name, :bakery
+    @@all = []
+
+    def initialize(name, bakery) 
+        @name = name
+        @bakery = bakery
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def ingredients 
+        Ingredient.all.select{ |ingredient| ingredient.dessert == self }
+    end
+
+end
+
+class Ingredient
+
+    attr_accessor :name, :dessert
+    @@all = []
+
+    def initialize(name, dessert) 
+        @name = name 
+        @dessert = dessert
+        @@all << self
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def bakery
+        self.dessert.bakery
+    end
+
+end
